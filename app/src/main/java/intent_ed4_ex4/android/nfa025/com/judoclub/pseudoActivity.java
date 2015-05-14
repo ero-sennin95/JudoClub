d05e7c67f9e41f39748573743d1cdffd6e48ed4d
david <davidxyz95bb@hotmail.com>
2015-05-14 13:49:05 +0200

    Premier commit

---
+++ app/src/main/java/intent_ed4_ex4/android/nfa025/com/judoclub/pseudoActivity.java
@@ -0,0 +1,18 @@
+package intent_ed4_ex4.android.nfa025.com.judoclub;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class pseudoActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pseudo);
+    }
+
+
+}
