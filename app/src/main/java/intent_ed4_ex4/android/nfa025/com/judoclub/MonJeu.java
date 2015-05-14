a1bbb80e0ab40aad23b719bad9dbd114513eb0be
david <davidxyz95bb@hotmail.com>
2015-05-14 15:17:59 +0200

    exo fini

--- app/src/main/java/intent_ed4_ex4/android/nfa025/com/judoclub/MonJeu.java
+++ app/src/main/java/intent_ed4_ex4/android/nfa025/com/judoclub/MonJeu.java
@@ -1,6 +1,7 @@
 package intent_ed4_ex4.android.nfa025.com.judoclub;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,6 +11,7 @@ public class MonJeu extends Activity {
 
     public static final String APP_TAG = "NFA025_EX4_TAG";
     public static final String TXT_PSEUDO = "PSEUDO_TXT";
+    public static final int RESULT_ID = 1;
 
     TextView pseudo_Tv;
     @Override
@@ -20,6 +22,7 @@ public class MonJeu extends Activity {
 
         if(null !=savedInstanceState){
             Log.i(APP_TAG,"Restaure etat précédent");
+            pseudo_Tv.setText(savedInstanceState.getString(TXT_PSEUDO));
         }
         else{
             Log.i(APP_TAG,"Premier démarrage");
@@ -29,10 +32,22 @@ public class MonJeu extends Activity {
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        //outState.putString(TXT_PSEUDO,pseudo_tv.getText().toString());
+        outState.putString(TXT_PSEUDO, pseudo_Tv.getText().toString());
     }
 
     public void identifyPlayer(View view){
         Log.i(APP_TAG,"identifyPlayer : Debut");
+        Intent pseudoIntent = new Intent(this,pseudoActivity.class);
+        pseudoIntent.putExtra(TXT_PSEUDO,pseudo_Tv.getText());
+        this.startActivityForResult(pseudoIntent,RESULT_ID);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.i(APP_TAG, "onActivityResult : Debut");
+        if (resultCode==RESULT_OK)
+            if (requestCode==RESULT_ID)
+                pseudo_Tv.setText(data.getStringExtra(TXT_PSEUDO));
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/intent_ed4_ex4/android/nfa025/com/judoclub/pseudoActivity.java
+++ app/src/main/java/intent_ed4_ex4/android/nfa025/com/judoclub/pseudoActivity.java
@@ -1,18 +1,52 @@
 package intent_ed4_ex4.android.nfa025.com.judoclub;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class pseudoActivity extends ActionBarActivity {
-
+    EditText pseudoET;
+    String oldPseudo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pseudo);
+        Intent result = this.getIntent();
+       oldPseudo =  result.getStringExtra(MonJeu.TXT_PSEUDO);
+        pseudoET =(EditText) this.findViewById(R.id.pseudoET);
+        pseudoET.setText(oldPseudo);
     }
 
+    public void onValidAction(View view){
+    Log.i(MonJeu.APP_TAG, "onValidAction(): Debut");
+        switch (view.getId()){
+            case R.id.OK_btn:
+                Log.i(MonJeu.APP_TAG, "OK Click");
+                String pseudo1=pseudoET.getText().toString();
+                if (pseudo1.equals(oldPseudo)) {
+                    Toast.makeText(this, "Le pseudo est le meme", Toast.LENGTH_SHORT).show();
+                    Log.i(MonJeu.APP_TAG, "OK Click, les chaine sont les meme");
+                    this.setResult(RESULT_CANCELED);
+                    break;
 
+                }
+                Intent result = new Intent();
+                result.putExtra(MonJeu.TXT_PSEUDO, pseudoET.getText().toString());
+                this.setResult(RESULT_OK,result);
+                break;
+            case R.id.Cancel_Btn:
+                Log.i(MonJeu.APP_TAG,"Cancel Click");
+                this.setResult(RESULT_CANCELED);
+                break;
+        }
+        this.finish();
+
+    }
 }
